34bf1dddb4dc1f779f391de73f74b24a53b97bee
Nezzx <nezzas001@gmail.com>
2026-05-08 19:50:13 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express    = require("express");
 const userRoutes = require("./routes/user.js");
+const cors = require("cors");
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
